08943f6a1ddeec01816ea993a1dc14303d52bf2b
lamifaharia <lamifaharia@gmail.com>
2026-08-20 00:35:47 +0600

    users

---
+++ src/module/user/user.routes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { getMe, getUsers,  } from "./user.controller";
+import auth from "../../middleware/auth";
+
+const userRouter = Router();
+
+userRouter.get("/me", auth(), getMe);
+userRouter.get("/", auth("ADMIN"), getUsers);
+
+
+
+
+export default userRouter;
